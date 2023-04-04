bf54720ac9ce5bd116758a5554bc21b6b2c406f4
SzabolcsGergely <szabi@luxonis.com>
2023-04-04 21:51:14 +0300

    Revert POE device stuck fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "57ca2c171f6ef53cb83fbbc3aabfe512c67796ce")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bcaf5410d20efbcba5e5845c521804bb6dfc76b5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
